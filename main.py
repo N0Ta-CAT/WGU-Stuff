9269e5d7e0c1d05cc812b871d1afc745a6f2b35c
Daric Wade <daric.wade@gmail.com>
2021-09-21 09:22:19 -0500

    Test Upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Main scratchpad for random stuff.
+
+SPNCharSet: list[str] = ['Sam', 'Dean', 'Castiel', 'Bobby']
+#what I get for watching SPN while I learn coding.
+for charStr in SPNCharSet:
+    print(charStr)
+
+#instructor showed how enumerate print the index position
+for i, charStr in enumerate(SPNCharSet):
+    print(charStr + ' is at position ' + str(i))
+
+
+
+
+
